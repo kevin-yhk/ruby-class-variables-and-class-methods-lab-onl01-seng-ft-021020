c02d81c3f842c6ad25b969f0c83b92d6cd83e781
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-27 00:19:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,7 @@ class Song
     @@artists.each do |artist|
       if artist_count[artist]
         artist_count[artist] += 1 
-      else artist_count[genre] = 1 
+      else artist_count[artist] = 1 
       end 
     end 
     artist_count 
