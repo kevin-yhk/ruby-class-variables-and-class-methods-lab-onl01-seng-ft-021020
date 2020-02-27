74a5eb88cd4eba2ebfb5e2661008def582a8855f
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-27 00:15:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,11 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
-        genre_count += 1 
-      else genre_count
+        genre_count[genre] += 1 
+      else genre_count[genre] = 1 
+      end 
+      genre_count
+    end 
+  end 
     
 end 
