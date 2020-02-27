71ec48fcce5dca40e39f568a5229f146354f2254
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-27 00:19:23 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,25 @@
-class Song  
-  attr_accessor :name, :artist, :genre
-    @@count = 0 
-    @@genres = []
-    @@artists = []
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
   def initialize(name, artist, genre)
-    @name = name 
+    @name = name
     @artist = artist
-    @genre =  genre
-    @@count += 1 
+    @genre = genre
+    @@count += 1
     @@genres << genre 
-    @@artists << artist
+    @@artists << artist 
   end 
   
   def self.count
     @@count
   end 
   
-  def self.genres 
-    @@genres.uniq
+  def self.genres
+    @@genres.uniq 
   end 
   
   def self.artists
@@ -26,26 +28,23 @@ class Song
   
   def self.genre_count
     genre_count = {}
-    @@genres.each do |i|
-      if genre_count[i] 
-        genre_count[i] += 1 
-      else 
-        genre_count[i] = 1 
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else genre_count[genre] = 1 
       end 
     end 
-      genre_count 
-    end 
-    
+    genre_count
+  end 
+   
   def self.artist_count
     artist_count = {}
-    @@artists.each do |i|
-      if artist_count[i]
-        artist_count[i] += 1 
-      else 
-        artist_count[i] = 1 
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else artist_count[artist] = 1 
       end 
     end 
-    artist_count
+    artist_count 
   end 
 end 
-
